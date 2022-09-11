6b60d550f0fa91b7fc9993c5d23a1409b507b6be
Andres Ruiz <ruizrobertoandres@gmail.com>
2022-09-11 19:04:45 -0300

    11/09

--- Ciervo3js/js/prueba.js
+++ Ciervo3js/js/prueba.js
@@ -2,8 +2,8 @@ document.addEventListener("mousemove", parallax)
     function parallax(e) {
         document.querySelectorAll(".object").forEach(function (move) {
             var moving_value = move.getAttribute("data-value");
-            var x = (e.clientX * moving_value) / 1000;
-            var y = (e.clientY * moving_value) / 1000;
+            var x = (e.clientX * moving_value) / 800;
+            var y = (e.clientY * moving_value) / 800;
             move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
         });
     }
